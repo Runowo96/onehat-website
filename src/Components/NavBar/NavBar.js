2284ae68830a1f914d31b7489d563b492c458096
runowo96 <devinmrunowski@gmail.com>
2024-05-15 10:43:48 -0400

    add logo in tab

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./NavBar.scss"
+import menuIcon from '../../assets/icons/menu-icon.svg'
 
-function Navbar() {
+function NavBar() {
   return (
     <div>
+        <img src={menuIcon} alt="menu icon" />
       
     </div>
   )
 }
 
-export default Navbar
+export default NavBar
