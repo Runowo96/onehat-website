287032376df0289b02904eebb3a5cbc0ca8e144e
runowo96 <devinmrunowski@gmail.com>
2024-05-19 16:51:13 -0400

    add desktop view

--- src/Components/HeroBanner/HeroBanner.js
+++ src/Components/HeroBanner/HeroBanner.js
@@ -9,7 +9,7 @@ function HeroBanner() {
         <div className="hero-banner__text-cont">
           <h1 className="hero-banner__title">Welcome</h1>
           <p className="hero-banner__description">
-            Stop buying a new hat when ever your kid grows, just buy ONEHAT
+            Stop buying a new hat when your kid grows, just buy ONEHAT
           </p>
         </div>
         <div className="hero-banner__img-cont">
