79dff4dcac7c535b2371bb8ae86ef7d4bcf328d8
runowo96 <devinmrunowski@gmail.com>
2024-05-20 16:27:23 -0400

    add mobile view for HowItWorks Component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroBanner from '../Components/HeroBanner/HeroBanner'
+import HowItWorks from '../Components/HowItWorks/HowItWorks'
 
 function HomePage() {
   return (
     <>
      <HeroBanner /> 
+     <HowItWorks />
     </>
   )
 }
