9f69b3a220305305a686edd784f4a9b5af78bf1b
runowo96 <devinmrunowski@gmail.com>
2024-05-20 16:44:11 -0400

    add tablet and desktop views for HowItWorks Component

--- src/Components/HowItWorks/HowItWorks.js
+++ src/Components/HowItWorks/HowItWorks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./HowItWorks.scss";
-import hatSelect from "../../assets/images/buckethat-colors-removebg-preview.png"
+import hatSelect from "../../assets/images/buckethat-colors-square.png"
 import patchSheet from "../../assets/images/Dino_Sheet-removebg-preview.png"
 import bucketHat from "../../assets/images/Untitled_design-removebg-preview.png"
 function HowItWorks() {
@@ -9,11 +9,11 @@ function HowItWorks() {
     <h2 className='how-it-works__header'>How it works</h2>
     <section className='how-it-works__sub-block-container'>
     <article className='how-it-works__sub-block'>
-        <h3 className='how-it-works__sub-block-header'>1. Select your favorite color</h3>
+        <h3 className='how-it-works__sub-block-header'>1. Select your color</h3>
     <img className='how-it-works__image' src={hatSelect} alt="selection of bucket hats in different colors" />
     </article>
     <article className='how-it-works__sub-block'>
-        <h3 className='how-it-works__sub-block-header'>2. Colect your patch sheets</h3>
+        <h3 className='how-it-works__sub-block-header'>2. Collect your patches</h3>
     <img className='how-it-works__image' src={patchSheet} alt="selection of bucket hats in different colors" />
     </article>
     <article className='how-it-works__sub-block'>
